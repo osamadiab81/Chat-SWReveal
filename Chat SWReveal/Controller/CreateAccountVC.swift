5c2d974f2867ad803533e8092591c74558119b77
osamadiab81 <osamadiab81@gmail.com>
2018-02-05 15:28:25 +0300

    now
    now

--- Chat SWReveal/Controller/CreateAccountVC.swift
+++ Chat SWReveal/Controller/CreateAccountVC.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
-
+    @IBOutlet weak var userNameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var userImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +21,23 @@ class CreateAccountVC: UIViewController {
 
     @IBAction func closeBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+    @IBAction func createAccountPresses(_ sender: Any) {
+        guard let email = emailTxt.text, emailTxt.text != "" else {
+            return
+        }
+        guard let paswword = passwordTxt.text, passwordTxt.text != "" else {
+            return
+        }
+        AuthService.instance.registerUser(email: email, password: paswword) { (success) in
+            if success {
+                print("yes")
+            }
+        }
+    }
+    @IBAction func chooseAvatarPressed(_ sender: Any) {
+    }
+    @IBAction func picBGColorPressed(_ sender: Any) {
     }
     
 }
--- Chat SWReveal/Utilities/Constance.swift
+++ Chat SWReveal/Utilities/Constance.swift
@@ -7,8 +7,21 @@
 //
 
 import Foundation
+
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL
+ let BASE_URL = "https://chatswreveal.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
 // Segues
     let TO_LOGIN = "logInSegue"
     let TO_CREATE_ACCOUNT = "creatAccountSegue"
     let UNWIND = "unwindToChannel"
+
+// Users Defualt
+
+    let TOKEN_KEY = "token"
+    let LOGGED_IN_KEY = "loggedIn"
+    let USER_EMAIL = "userEmail"
+
 
